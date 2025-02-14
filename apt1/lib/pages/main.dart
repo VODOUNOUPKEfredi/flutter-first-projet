aa5ea92e4693279d74247e45dc1d188b49443874
vodounoukpe <fvodounoupke@gmail.com>
2025-02-14 17:36:56 +0100

    reprise terminer ajout de categories,REcommendation,REcipes of the week,bottomNavigationbar plus la navigation entre les page grace au index

---
+++ apt1/lib/pages/main.dart
@@ -0,0 +1,63 @@
+import 'package:apt1/pages/details.dart';
+import 'package:flutter/material.dart';
+//import 'package:apt1/pages/main.dart';
+import 'package:apt1/pages/profil.dart';
+import 'package:apt1/pages/ajout.dart';
+import 'package:apt1/pages/search.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      colorSchemeSeed: Colors.green,
+      scaffoldBackgroundColor: Colors.white,
+    ),
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() {
+    return HomePageStates();
+  }
+}
+
+class HomePageStates extends State<HomePage> {
+  final pages = [
+    HomePage(),
+    searchPage(),
+    AjoutPage(),
+    DetailsPage(),
+    Profilpage(),
+  ];
+  int indexpage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    EdgeInsets.all(40);
+
+    EdgeInsets.only(bottom: 20);
+
+    return Scaffold(
+      body: pages[indexpage],
+      bottomNavigationBar: NavigationBar(
+          selectedIndex: indexpage,
+          onDestinationSelected: (int index) {
+            setState(() {
+              indexpage = index;
+            });
+          },
+          backgroundColor: Colors.white,
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: ""),
+            NavigationDestination(icon: Icon(Icons.search), label: ""),
+            NavigationDestination(icon: Icon(Icons.add), label: ""),
+            NavigationDestination(icon: Icon(Icons.bookmark), label: ""),
+            NavigationDestination(icon: Icon(Icons.person), label: ""),
+          ]),
+    );
+  }
+}
