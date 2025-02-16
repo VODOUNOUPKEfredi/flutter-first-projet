45002d7563c31470e35b9c26c6f4eddc22fbf376
vodounoukpe <fvodounoupke@gmail.com>
2025-02-16 19:26:46 +0100

    page vieww mise en place

--- apt1/lib/pages/bookmark.dart
+++ apt1/lib/pages/bookmark.dart
@@ -21,16 +21,52 @@ class BookmarkPage extends StatefulWidget {
 }
 
 class DetailsPageStates extends State<BookmarkPage> {
-  @override
+  
+ final List<Map<String, dynamic>> images = [
+    {"image": "https://img.freepik.com/free-photo/brazilian-man-having-guarana-drink-outdoors_23-2150765674.jpg?ga=GA1.1.1512817245.1739357619"},
+     {"image": "https://img.freepik.com/free-photo/brazilian-man-having-guarana-drink-outdoors_23-2150765674.jpg?ga=GA1.1.1512817245.1739357619"},
+      {"image": "https://img.freepik.com/free-photo/brazilian-man-having-guarana-drink-outdoors_23-2150765674.jpg?ga=GA1.1.1512817245.1739357619"},
+       {"image": "https://img.freepik.com/free-photo/brazilian-man-having-guarana-drink-outdoors_23-2150765674.jpg?ga=GA1.1.1512817245.1739357619"}
+  ];
+  int indeximage = 0;
+ @override
   Widget build(BuildContext context) {
     EdgeInsets.all(40);
 
     EdgeInsets.only(bottom: 20);
-
+ //double screenWidth = MediaQuery.of(context).size.width;
+ //   double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body:  Center(
-      child: Text("bookmark"),
+      body:  SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+              
+              ),
+              child: 
+              ListView(
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        children: [
+                          Text("Choco Macaron", style: TextStyle(color: Colors.black)),
+                          Text("By Rachelle")
+                        ],
+                      )
+                    ],
+                    
+                  )
+                ],
+              ),
+            )
+          ],
+
+          
+        ),
       ),
     );
   }
-}
+  }
+
