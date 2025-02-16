12f6fde416bb453ed82c62ed411bb8475a6622ef
vodounoukpe <fvodounoupke@gmail.com>
2025-02-16 19:25:40 +0100

    responsivité mise en place

--- apt1/lib/pages/accueil.dart
+++ apt1/lib/pages/accueil.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 //import 'package:apt1/pages/details.dart';
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(
-      colorSchemeSeed: Colors.green,
-      scaffoldBackgroundColor: Colors.white,
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        colorSchemeSeed: Colors.green,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: AcceuilPage(),
     ),
-    home: AcceuilPage(),
-  ));
+  );
 }
 
 class AcceuilPage extends StatefulWidget {
@@ -22,10 +24,10 @@ class AcceuilPage extends StatefulWidget {
 
 class AcceuilPageStates extends State<AcceuilPage> {
   final List<Map<String, dynamic>> categories = [
-    {"name": "Breakfast", "image": 'asset/image/1.jpeg'},
-    {"name": "Luch", "image": 'asset/image/2.jpeg'},
-    {"name": "Dinner", "image": 'asset/image/3.jpeg'},
-    {"name": "Dessert", "image": 'asset/image/2.jpeg'},
+    {"name": "Breakfast", "image": 'images1/1.png'},
+    {"name": "Luch", "image": 'images1/1.png'},
+    {"name": "Dinner", "image": 'images1/1.png'},
+    {"name": "Dessert", "image": 'images1/1.png'},
   ];
   int? selectedIndex = 0;
   @override
@@ -33,172 +35,120 @@ class AcceuilPageStates extends State<AcceuilPage> {
     EdgeInsets.all(40);
 
     EdgeInsets.only(bottom: 20);
-
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 12,
-          title: const Text(
-            "hello",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 12,
+        title: const Text(
+          "hello",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        actions: const [
+          CircleAvatar(
+            backgroundColor: Colors.grey,
+            child: Icon(Icons.person, color: Colors.white),
           ),
-          actions: const [
-            CircleAvatar(
-              backgroundColor: Colors.grey,
-              child: Icon(
-                Icons.person,
-                color: Colors.white,
+          SizedBox(width: 10),
+        ],
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                labelText: "recherche",
+                prefixIcon: Icon(Icons.search),
+                suffixIcon: Icon(Icons.filter_1_outlined),
               ),
             ),
-            SizedBox(
-              width: 15,
-            )
-          ],
-        ),
-        body: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            padding: EdgeInsets.all(20),
-            child: Column(
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    labelText: "recherche",
-                    prefixIcon: Icon(Icons.search),
-                    suffixIcon: Icon(Icons.filter_1_outlined),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Categories"),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Text("See all"),
-                  ],
-                ),
-                Column(
-                  children: [
-                    SizedBox(
-                      height: 150,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: categories.length,
-                        itemBuilder: (context, index) => GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                selectedIndex = index;
-                              });
-                            },
-                            child: Container(
-                                height: 200,
-                                width: 100,
-                                margin: EdgeInsets.only(right: 10),
-                                decoration: BoxDecoration(
-                                  boxShadow: [
-                                    BoxShadow(
-                                        color: Colors.grey,
-                                        spreadRadius: 2,
-                                        blurRadius: 5)
-                                  ],
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: selectedIndex == index
-                                      ? Colors.green
-                                      : Colors.white,
-                                ),
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    Image.network(
-                                      categories[index]["image"]!,
-                                      height:100,
-                                      width: 100,
-                                      fit: BoxFit.cover,
-                                    ),
-                                    SizedBox(
-                                      height: 15,
-                                      width: 20,
-                                    ),
-                                    Text(categories[index]["name"]!,
-                                        style: TextStyle(
-                                          fontSize: 10,
-                                          fontWeight: FontWeight.bold,
-                                        ))
-                                  ],
-                                ))),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    SizedBox(
-                      height: 150,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: categories.length,
-                        itemBuilder: (context, index) => GestureDetector(
-                            child: Container(
-                                height: 200,
-                                width: 100,
-                                margin: EdgeInsets.only(right: 10),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                   Container(
-                                    width: 300,
-                                    height: 100,
-                                    child:  Image.network(
-                                      categories[index]["image"]!,
-                                      //height: 100,
-                                      //width: 200,
-                                      fit: BoxFit.cover,
-                                    ),
-                                   ),
-                                    SizedBox(
-                                      height: 20,
-                                      //width: 20,
-                                    ),
-                                    Text(categories[index]["name"]!,
-                                        style: TextStyle(
-                                          fontSize: 10,
-                                          fontWeight: FontWeight.bold,
-                                        ))
-                                  ],
-                                ))),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(height: 30,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Recipes of the Week"),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text("See all",style: TextStyle(color: Colors.green),),
-                  ],
-                ),
+                Text("Categories"),
+                SizedBox(height: screenWidth * 0.05),
+                Text("See all"),
+              ],
+            ),
+            SizedBox(height: 20),
+            Column(
+              children: [
                 SizedBox(
-                  height: 150,
-                  width: 300,
+                  height: screenHeight * 0.3,
+                  width: screenWidth * 1,
                   child: ListView.builder(
                     scrollDirection: Axis.horizontal,
                     itemCount: categories.length,
-                    itemBuilder: (context, index) => GestureDetector(
-                        child: Container(
-                            height: 200,
-                            width: 100,
+                    itemBuilder:
+                        (context, index) => GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedIndex = index;
+                            });
+                          },
+                          child: Container(
+                            height: screenHeight * 0.5,
+                            width: screenWidth * 0.3,
+                            margin: EdgeInsets.all(screenWidth * 0.05),
+                            padding: EdgeInsets.all(screenWidth * 0.01),
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey,
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                ),
+                              ],
+                              borderRadius: BorderRadius.circular(10),
+                              color:
+                                  selectedIndex == index
+                                      ? Colors.green
+                                      : Colors.white,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Image.network(
+                                  categories[index]["image"]!,
+                                  height: screenHeight * 0.2,
+                                  width: screenWidth * 0.05,
+                                  //fit: BoxFit.cover,
+                                ),
+                                SizedBox(
+                                  height: screenHeight * 0.1,
+                                  width: screenWidth * 0.05,
+                                ),
+                                Text(
+                                  categories[index]["name"]!,
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                  ),
+                ),
+                SizedBox(height: screenWidth * 0.02),
+                SizedBox(
+                  height: screenHeight * 0.3,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: categories.length,
+                    itemBuilder:
+                        (context, index) => GestureDetector(
+                          child: Container(
+                            height: screenHeight * 0.1,
+                            width: screenWidth * 0.4,
                             margin: EdgeInsets.only(right: 10),
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(10),
@@ -206,26 +156,76 @@ class AcceuilPageStates extends State<AcceuilPage> {
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
-                                Image.network(
-                                  categories[index]["image"]!,
-                                  height: 50,
-                                  width: 30,
-                                  fit: BoxFit.cover,
+                                SizedBox(
+                                  width: screenHeight * 0.3,
+                                  height: screenWidth * 0.3,
+                                  child: Image.network(
+                                    categories[index]["image"]!,
+                                    //height: 100,
+                                    //width: 200,
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                                 SizedBox(
-                                  height: 15,
-                                  width: 20,
+                                  height: screenHeight * 0.05,
+                                  width: screenHeight * 0.1,
                                 ),
-                                Text(categories[index]["name"]!,
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.bold,
-                                    ))
+                                Text(
+                                  categories[index]["name"]!,
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ],
-                            ))),
+                            ),
+                          ),
+                        ),
                   ),
-                )
+                ),
               ],
-            )));
+            ),
+            SizedBox(height: screenWidth * 0.05),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Recipes of the Week"),
+                SizedBox(height: screenHeight * 0.05),
+                Text("See all", style: TextStyle(color: Colors.green)),
+              ],
+            ),
+            SizedBox(
+              height: screenHeight * 0.8,
+              width: screenWidth * 1,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder:
+                    (context, index) => GestureDetector(
+                      child: Column(
+                        children: [
+                          Container(
+                            height: screenHeight * 0.3,
+                            width: screenWidth * 0.7,
+                            margin: EdgeInsets.only(right: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Image.network(
+                              categories[index]["image"]!,
+                              // height: screenHeight*0.8,
+                              // width: screenWidth*0.5,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
